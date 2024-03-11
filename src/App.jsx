9e11d8b1aaec0e5c2f42293a0b5d5903d28459a6
pablotoledo87 <toledo.pablo87@gmail.com>
2024-03-11 20:53:38 -0300

    agregamos las rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import Footer from './components/common/Footer'
 import Inicio from './components/pages/Inicio'
 import Administrador from './components/pages/Administrador';
 import Error404 from './components/pages/Error404';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
  
 
   return (
-    <>
-      
-     <Menu></Menu>
-     <Inicio></Inicio>
-     {/* <Administrador></Administrador> */}
-     {/* <Error404></Error404> */}
-     <Footer></Footer>
-    </>
+  <BrowserRouter>
+  <Menu></Menu>
+  <Routes>
+    <Route exact path='/' element={<Inicio></Inicio>}></Route>
+    <Route exact path='/administrador' element={<Administrador></Administrador>}></Route>
+    <Route exact path='/administrador/crear' element={<Administrador></Administrador>}></Route>
+    <Route exact path='/administrador/editar' element={<Administrador></Administrador>}></Route>
+    <Route exact path='*' element={<Error404></Error404>}></Route>
+  </Routes>
+  <Footer></Footer>
+  </BrowserRouter>
   )
 }
 
